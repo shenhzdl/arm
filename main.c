8ab654732ab7e1545be224ce84d26ecabbe6217a
shenhzdl <shenhzdl@gmail.com>
2017-11-09 13:47:26 +0800

    Update main.c
    test

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
 //test
+#include <configs/ti_am335x_common.h>
+
+#define MACH_TYPE_TIAM335EVM		3589	/* Until the next sync */
+#define CONFIG_MACH_TYPE		MACH_TYPE_TIAM335EVM
+
+#define CONFIG_CMD_SPL_WRITE_SIZE 0x400
+
+#define CONFIG_MII
